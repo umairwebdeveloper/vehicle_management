4faee51f048fdbe76fe2facf211c46d4b5841986
Umair Ashraf <umairashraf5252@gmail.com>
2025-05-05 14:32:20 +0500

    fixed

--- carminder/urls.py
+++ carminder/urls.py
@@ -33,6 +33,8 @@ urlpatterns = [
     path("forum/", include("forum.urls")),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
